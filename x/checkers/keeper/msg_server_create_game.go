4e1c9cf3c591bd1404e342433806fa91420c0dd1
Krati Jain <kratijain88@gmail.com>
2022-08-01 02:28:28 +0530

    updated create game

--- x/checkers/keeper/msg_server_create_game.go
+++ x/checkers/keeper/msg_server_create_game.go
@@ -2,16 +2,40 @@ package keeper
 
 import (
 	"context"
+	"strconv"
 
-	"github.com/alice/checkers/x/checkers/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	rules "github.com/alice/checkers/x/checkers/rules"
+	"github.com/alice/checkers/x/checkers/types"
 )
 
 func (k msgServer) CreateGame(goCtx context.Context, msg *types.MsgCreateGame) (*types.MsgCreateGameResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	nextGame, found := k.Keeper.GetNextGame(ctx)
+	if !found {
+		panic("NextGame not found")
+	}
+	newIndex := strconv.FormatUint(nextGame.IdValue, 10)
+	newGame := rules.New()
+	storedGame := types.StoredGame{
+		Creator: msg.Creator,
+		Index:   newIndex,
+		Game:    newGame.String(),
+		Turn:    rules.PieceStrings[newGame.Turn],
+		Red:     msg.Red,
+		Black:   msg.Black,
+	}
+	err := storedGame.Validate()
+	if err != nil {
+		return nil, err
+	}
+	k.Keeper.SetStoredGame(ctx, storedGame)
 
-	return &types.MsgCreateGameResponse{}, nil
+	nextGame.IdValue++
+	k.Keeper.SetNextGame(ctx, nextGame)
+
+	return &types.MsgCreateGameResponse{
+		IdValue: newIndex,
+	}, nil
 }
---
+++ x/checkers/keeper/msg_server_create_game_test.go
@@ -0,0 +1,33 @@
+package keeper_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/alice/checkers/x/checkers"
+)
+
+const (
+	alice = "cosmos1jmjfq0tplp9tmx4v9uemw72y4d2wa5nr3xn9d3"
+	bob   = "cosmos1xyxs3skf3f4jfqeuv89yyaqvjc6lffavxqhc8g"
+	carol = "cosmos1e0w5t53nrq7p66fye6c8p0ynyhf6y24l4yuxd7"
+)
+
+func setupMsgServerCreateGame(t testing.TB) (types.MsgServer, keeper.Keeper, context.Context) {
+	k, ctx := setupKeeper(t)
+	checkers.InitGenesis(ctx, *k, *types.DefaultGenesis())
+	return keeper.NewMsgServerImpl(*k), *k, sdk.WrapSDKContext(ctx)
+}
+
+func TestCreateGame(t *testing.T) {
+	msgServer, context := setupMsgServerCreateGame(t)
+	createResponse, err := msgServer.CreateGame(context, &types.MsgCreateGame{
+		Creator: alice,
+		Red:     bob,
+		Black:   carol,
+	})
+	require.Nil(t, err)
+	require.EqualValues(t, types.MsgCreateGameResponse{
+		IdValue: "1", // TODO: update with a proper value when updated
+	}, *createResponse)
+}
