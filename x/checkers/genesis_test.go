da94d28a57c1599056bb7904fa318c7130896eac
Krati Jain <kratijain88@gmail.com>
2022-07-31 20:45:24 +0530

    Added nextGame and stored_game

--- x/checkers/genesis_test.go
+++ x/checkers/genesis_test.go
@@ -14,6 +14,18 @@ func TestGenesis(t *testing.T) {
 	genesisState := types.GenesisState{
 		Params: types.DefaultParams(),
 
+		NextGame: &types.NextGame{
+			IdValue: 8,
+			Creator: "8",
+		},
+		StoredGameList: []types.StoredGame{
+			{
+				Index: "0",
+			},
+			{
+				Index: "1",
+			},
+		},
 		// this line is used by starport scaffolding # genesis/test/state
 	}
 
@@ -25,5 +37,7 @@ func TestGenesis(t *testing.T) {
 	nullify.Fill(&genesisState)
 	nullify.Fill(got)
 
+	require.Equal(t, genesisState.NextGame, got.NextGame)
+	require.ElementsMatch(t, genesisState.StoredGameList, got.StoredGameList)
 	// this line is used by starport scaffolding # genesis/test/assert
 }
