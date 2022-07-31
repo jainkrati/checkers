acbb4ec80a979ac7fde7ba4432913fe2e6a37427
Krati Jain <kratijain88@gmail.com>
2022-07-31 20:38:25 +0530

    Revert "added idValue"
    This reverts commit a5a0be5bfb14d02040b94c5fb74e75a1458961e8.

--- vue/src/store/generated/alice/checkers/alice.checkers.checkers/index.ts
+++ vue/src/store/generated/alice/checkers/alice.checkers.checkers/index.ts
@@ -1,10 +1,9 @@
 import { txClient, queryClient, MissingWalletError , registry} from './module'
 
-import { NextGame } from "./module/types/checkers/next_game"
 import { Params } from "./module/types/checkers/params"
 
 
-export { NextGame, Params };
+export { Params };
 
 async function initTxClient(vuexGetters) {
 	return await txClient(vuexGetters['common/wallet/signer'], {
@@ -43,10 +42,8 @@ function getStructure(template) {
 const getDefaultState = () => {
 	return {
 				Params: {},
-				NextGame: {},
 				
 				_Structure: {
-						NextGame: getStructure(NextGame.fromPartial({})),
 						Params: getStructure(Params.fromPartial({})),
 						
 		},
@@ -81,12 +78,6 @@ export default {
 						(<any> params).query=null
 					}
 			return state.Params[JSON.stringify(params)] ?? {}
-		},
-				getNextGame: (state) => (params = { params: {}}) => {
-					if (!(<any> params).query) {
-						(<any> params).query=null
-					}
-			return state.NextGame[JSON.stringify(params)] ?? {}
 		},
 				
 		getTypeStructure: (state) => (type) => {
@@ -139,28 +130,6 @@ export default {
 				return getters['getParams']( { params: {...key}, query}) ?? {}
 			} catch (e) {
 				throw new Error('QueryClient:QueryParams API Node Unavailable. Could not perform query: ' + e.message)
-				
-			}
-		},
-		
-		
-		
-		
-		 		
-		
-		
-		async QueryNextGame({ commit, rootGetters, getters }, { options: { subscribe, all} = { subscribe:false, all:false}, params, query=null }) {
-			try {
-				const key = params ?? {};
-				const queryClient=await initQueryClient(rootGetters)
-				let value= (await queryClient.queryNextGame()).data
-				
-					
-				commit('QUERY', { query: 'NextGame', key: { params: {...key}, query}, value })
-				if (subscribe) commit('SUBSCRIBE', { action: 'QueryNextGame', payload: { options: { all }, params: {...key},query }})
-				return getters['getNextGame']( { params: {...key}, query}) ?? {}
-			} catch (e) {
-				throw new Error('QueryClient:QueryNextGame API Node Unavailable. Could not perform query: ' + e.message)
 				
 			}
 		},
--- vue/src/store/generated/alice/checkers/alice.checkers.checkers/module/types/checkers/genesis.ts
+++ vue/src/store/generated/alice/checkers/alice.checkers.checkers/module/types/checkers/genesis.ts
@@ -1,15 +1,13 @@
 /* eslint-disable */
 import { Params } from "../checkers/params";
-import { NextGame } from "../checkers/next_game";
 import { Writer, Reader } from "protobufjs/minimal";
 
 export const protobufPackage = "alice.checkers.checkers";
 
 /** GenesisState defines the checkers module's genesis state. */
 export interface GenesisState {
-  params: Params | undefined;
   /** this line is used by starport scaffolding # genesis/proto/state */
-  nextGame: NextGame | undefined;
+  params: Params | undefined;
 }
 
 const baseGenesisState: object = {};
@@ -18,9 +16,6 @@ export const GenesisState = {
   encode(message: GenesisState, writer: Writer = Writer.create()): Writer {
     if (message.params !== undefined) {
       Params.encode(message.params, writer.uint32(10).fork()).ldelim();
-    }
-    if (message.nextGame !== undefined) {
-      NextGame.encode(message.nextGame, writer.uint32(18).fork()).ldelim();
     }
     return writer;
   },
@@ -34,9 +29,6 @@ export const GenesisState = {
       switch (tag >>> 3) {
         case 1:
           message.params = Params.decode(reader, reader.uint32());
-          break;
-        case 2:
-          message.nextGame = NextGame.decode(reader, reader.uint32());
           break;
         default:
           reader.skipType(tag & 7);
@@ -53,11 +45,6 @@ export const GenesisState = {
     } else {
       message.params = undefined;
     }
-    if (object.nextGame !== undefined && object.nextGame !== null) {
-      message.nextGame = NextGame.fromJSON(object.nextGame);
-    } else {
-      message.nextGame = undefined;
-    }
     return message;
   },
 
@@ -65,10 +52,6 @@ export const GenesisState = {
     const obj: any = {};
     message.params !== undefined &&
       (obj.params = message.params ? Params.toJSON(message.params) : undefined);
-    message.nextGame !== undefined &&
-      (obj.nextGame = message.nextGame
-        ? NextGame.toJSON(message.nextGame)
-        : undefined);
     return obj;
   },
 
@@ -78,11 +61,6 @@ export const GenesisState = {
       message.params = Params.fromPartial(object.params);
     } else {
       message.params = undefined;
-    }
-    if (object.nextGame !== undefined && object.nextGame !== null) {
-      message.nextGame = NextGame.fromPartial(object.nextGame);
-    } else {
-      message.nextGame = undefined;
     }
     return message;
   },
